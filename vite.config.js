812d169e5f0daa1845342a2943e567fb0ba92f52
Jeff Jassky <jeff@jeffjassky.com>
2025-03-18 20:47:27 -0400

    trying this instead

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [vue(), viteSingleFile()],
+  plugins: [vue()],
   server: {
     port: 5000,
     host: true,
@@ -14,12 +14,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    assetsInlineLimit: 100000000, // Large enough to inline assets
-    cssCodeSplit: false, // Avoids splitting CSS into separate file
-    rollupOptions: {
-		output: {
-		  inlineDynamicImports: true, // Inline dynamic imports
-		},
-	  },
+    // assetsInlineLimit: 100000000, // Large enough to inline assets
+    // cssCodeSplit: false, // Avoids splitting CSS into separate file
+    // rollupOptions: {
+	// 	output: {
+	// 	  inlineDynamicImports: true, // Inline dynamic imports
+	// 	},
+	//   },
   }
 });

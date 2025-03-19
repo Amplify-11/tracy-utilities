d23b697460d06854d25ed2d0e10abde7edb0791c
Jeff Jassky <jeff@jeffjassky.com>
2025-03-18 20:59:53 -0400

    okay lets try this

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  base: '/tracy-utilities/', // Top-level base config for GitHub Pages
   plugins: [vue()],
   server: {
     port: 5000,
     host: true,
     hmr: true,
     strictPort: true,
-	allowedHosts: true
+    allowedHosts: true
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    // assetsInlineLimit: 100000000, // Large enough to inline assets
-    // cssCodeSplit: false, // Avoids splitting CSS into separate file
-    // rollupOptions: {
-	// 	output: {
-	// 	  inlineDynamicImports: true, // Inline dynamic imports
-	// 	},
-	//   },
+    assetsDir: '.',
   }
 });
